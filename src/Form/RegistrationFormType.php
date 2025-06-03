75f0fafadf6b1d88e9c8d38f3520759c011f897a
Hamish Elliot <hamishelliot@Hamishs-MacBook-Pro.local>
2025-06-03 14:03:22 +1200

    feat; registrationFormtype

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email Address',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'First Name',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Last Name',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('studentId', TextType::class, [
+                'label' => 'Student ID (Optional)',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Account Type',
+                'choices' => [
+                    'Student' => 'ROLE_STUDENT',
+                    'Administrator' => 'ROLE_ADMIN',
+                ],
+                'data' => 'ROLE_STUDENT',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => ['class' => 'form-control', 'autocomplete' => 'new-password'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirm Password',
+                    'attr' => ['class' => 'form-control', 'autocomplete' => 'new-password'],
+                ],
+                'invalid_message' => 'The password fields must match.',
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter a password']),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password must be at least {{ limit }} characters long.',
+                        'max' => 4096
+                    ]),
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'I have read and agree to the terms and conditions',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue(['message' => 'You must agree to the terms and conditions.']),
+                ],
+                'attr' => ['class' => 'form-check-input']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
