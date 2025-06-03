cb5599a578191c696bdf5861d950e0a707c7b618
Hamish Elliot <hamishelliot@Hamishs-MacBook-Pro.local>
2025-06-03 17:09:58 +1200

    feat: dashboardController setup

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,18 +1,55 @@
 <?php
+// src/Controller/DashboardController.php
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-final class DashboardController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('dashboard/home.html.twig');
+    }
+
+    #[Route('/dashboard', name: 'app_dashboard')]
+    #[IsGranted('ROLE_USER')]
+    public function dashboard(): Response
+    {
+        $user = $this->getUser();
+
+        if ($user->isAdmin()) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/admin', name: 'admin_dashboard')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function adminDashboard(): Response
+    {
+        return $this->render('dashboard/admin.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    #[IsGranted('ROLE_USER')]
+    public function profile(): Response
+    {
+        return $this->render('dashboard/profile.html.twig', [
+            'user' => $this->getUser(),
         ]);
     }
 }
