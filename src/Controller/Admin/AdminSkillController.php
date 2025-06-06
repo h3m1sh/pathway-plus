2edaabd2314a9534ecf6de689d19d00270667c2c
Hamish Elliot <hamishelliot@Hamishs-MacBook-Pro.local>
2025-06-06 12:59:51 +1200

    remove: skill show function

--- src/Controller/Admin/AdminSkillController.php
+++ src/Controller/Admin/AdminSkillController.php
@@ -52,32 +52,8 @@ final class AdminSkillController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_skill_show', methods: ['GET', 'POST'])]
-    public function show(int $id, SkillRepository $skillRepository, Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $skill = $skillRepository->find($id);
-
-        if (!$skill) {
-            throw $this->createNotFoundException('Skill not found');
-        }
-
-        $form = $this->createForm(SkillFormType::class, $skill);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($skill);
-            $entityManager->flush();
-            return $this->redirectToRoute('app_admin_skill_index');
-        }
-
-        return $this->render('admin/skill/skillForm.html.twig', [
-            'skill' => $skill,
-            'form' => $form->createView(),
-        ]);
-    }
-
     #[Route('/edit/{id}', name: 'app_admin_skill_edit', methods: ['GET', 'POST'])]
-    public function edit(int $id,SkillRepository $skillRepository, Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(int $id, SkillRepository $skillRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $skill = $skillRepository->find($id);
 
@@ -98,8 +74,6 @@ final class AdminSkillController extends AbstractController
             'skill' => $skill,
             'form' => $form->createView(),
         ]);
-
-
     }
 
     #[Route('/delete/{id}', name: 'app_admin_skill_delete', methods: ['DELETE'])]
@@ -107,15 +81,12 @@ final class AdminSkillController extends AbstractController
     {
         $skill = $skillRepository->find($id);
 
-
         if (!$skill) {
             throw $this->createNotFoundException('Skill not found');
         }
 
-
         $entityManager->remove($skill);
         $entityManager->flush();
-        return $this->redirectToRoute('app_admin_skill');
-
+        return $this->redirectToRoute('app_admin_skill_index');
     }
 }
