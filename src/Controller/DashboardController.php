46f15481e52e6fa690f51d678960599bec653108
hamishelliot <hamish.elliot5@gmail.com>
2025-06-12 12:49:52 +1200

    edit: add student-specific data in dashboardController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,10 +3,12 @@
 
 namespace App\Controller;
 
+use App\Repository\StudentProgressRepository;
 use App\Repository\UserRepository;
 use App\Repository\SkillRepository;
 use App\Repository\JobRoleRepository;
 use App\Repository\MicroCredentialRepository;
+use App\Entity\StudentProgress;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -26,12 +28,35 @@ class DashboardController extends AbstractController
 
     #[Route('/dashboard', name: 'app_dashboard')]
     #[IsGranted('ROLE_USER')]
-    public function dashboard(): Response
+    public function dashboard(StudentProgressRepository $studentProgressRepository,
+        JobRoleRepository $jobRoleRepository): Response
     {
         $user = $this->getUser();
 
-        return $this->render('dashboard/index.html.twig', [
+        if ($user->isAdmin()) {
+            return $this->redirectToRoute('app_admin_dashboard');
+        }
+
+        $studentProgress = $studentProgressRepository->findBy(['student' => $user], ['dataEarned' => 'DESC']);
+        $recentProgress = $studentProgressRepository->findRecentProgress($user, 30);
+        $careerInterests = $user->getJobRoleInterests();
+
+        $totalCredentials = count($studentProgress);
+        $completedCredentials = count(array_filter($studentProgress, fn($p) => $p->isCompleted()));
+        $completionRate = $totalCredentials > 0 ? round(($completedCredentials / $totalCredentials) * 100) : 0;
+
+
+        return $this->render('dashboard/student.html.twig', [
             'user' => $user,
+            'studentProgress' => $studentProgress,
+            'recentProgress' => $recentProgress,
+            'careerInterests' => $careerInterests,
+            'stats' => [
+                'totalCredentials' => $totalCredentials,
+                'completedCredentials' => $completedCredentials,
+                'completionRate' => $completionRate,
+                'careerGoals' => count($careerInterests),
+            ]
         ]);
     }
 
